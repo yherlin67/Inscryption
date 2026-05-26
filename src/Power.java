48687330ba31487826067205702fe6f010dce609
Yanis Herlin <yherlin@unistra.fr>
2026-05-26 14:06:20 +0200

    Ajout classe pouvoir

---
+++ src/Power.java
@@ -0,0 +1,12 @@
+package inscryption;
+
+public class Power {
+
+    private boolean m_nombreusesVies;
+    private boolean m_croissance;
+    private boolean m_puant;
+    private boolean m_coureur;
+    private boolean m_contactMortel;
+    private boolean m_piquesPointues;
+}
+
--- src/inscryption/cartes/Cartes_animaux.java
+++ src/inscryption/cartes/Cartes_animaux.java
@@ -7,6 +7,7 @@ public abstract class Cartes_animaux extends Cartes{
     private final int m_blood;
     private final int m_bone;
     private final boolean m_flying;
+    private Power m_power;
 
     public Cartes_animaux(String nom, int att, int pdv, int gds, int os, boolean vol)
     {
@@ -32,5 +33,10 @@ public abstract class Cartes_animaux extends Cartes{
 
     public boolean isFlying() {
         return m_flying;
+    }
+
+    public void appliquerPouvoir()
+    {
+        //switch
     }
 }
